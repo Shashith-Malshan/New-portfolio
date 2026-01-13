c75f162fe7aec83829dc0b10fab587755c9a7a89
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-13 20:40:22 +0530

    Create Projects Section

--- src/app/page/contact/contact.component.ts
+++ src/app/page/contact/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../component/navbar/navbar.component';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [NavbarComponent,FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
--- src/app/page/projects/projects.component.ts
+++ src/app/page/projects/projects.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../component/navbar/navbar.component';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [NavbarComponent,FooterComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
