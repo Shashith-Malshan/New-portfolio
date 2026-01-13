0f00986ba0b3ea5f336f22026a9600a8e8d71daf
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-13 20:31:06 +0530

    Create Home Section

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../component/navbar/navbar.component';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,FooterComponent,HomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
