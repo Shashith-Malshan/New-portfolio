eae824b49d49f02bbe6c2542710638a3cbafce6f
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-13 19:57:20 +0530

    Add App routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { ProjectsComponent } from './page/projects/projects.component';
+import { ContactComponent } from './page/contact/contact.component';
 
 export const routes: Routes = [
 
-    
+    {
+        path:"",
+        component:HomeComponent
+    },
+    {
+        path:"contact",
+        component:ContactComponent
+    },
+    {
+        path:"projects",
+        component:ProjectsComponent
+    }
 
 ];
--- src/app/page/projects/projects.component.spec.ts
+++ src/app/page/projects/projects.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PojectsComponent } from './pojects.component';
+import { ProjectsComponent } from './projects.component';
 
-describe('PojectsComponent', () => {
-  let component: PojectsComponent;
-  let fixture: ComponentFixture<PojectsComponent>;
+describe('ProjectsComponent', () => {
+  let component: ProjectsComponent;
+  let fixture: ComponentFixture<ProjectsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PojectsComponent]
+      imports: [ProjectsComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(PojectsComponent);
+    fixture = TestBed.createComponent(ProjectsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
